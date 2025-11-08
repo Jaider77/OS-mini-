be9ef3539a5e2afe2c09e0472edb09904cf45689
Jaider Aprilla <jaiderreyes273@gmail.com>
2025-11-08 10:09:21 -0500

    se agreggarosn los modulos sobre los cuale vamos a trabajar

---
+++ modulos/__init__.py
@@ -0,0 +1,3 @@
+# Este archivo se deja intencionalmente vacío.
+# Sirve para que Python reconozca esta carpeta como un "paquete".
+# Puedes agregar inicializaciones de paquete aquí si es necesario en el futuro.
